c5ed042bcc13e3174059085a1cb20c22d14cb8e0
Mahmoud-dev09 <mahmoud0114829@gmail.com>
2025-07-13 14:49:28 +0300

    Refactor recipe rendering logic and update API endpoint in controller.js
    - Updated API URL to use the new forkify endpoint.
    - Refactored recipe data extraction and rendering for clarity.
    - Improved error handling and loading spinner.
    - Added markup for recipe details, servings, and ingredients.
    - Fixed minor issues in the recipe rendering flow.

---
+++ src/js/icons.js
@@ -0,0 +1,23 @@
+// Icon references for the SVG sprite
+const icons = {
+  clock: 'src/img/icons.svg#icon-clock',
+  users: 'src/img/icons.svg#icon-users',
+  minusCircle: 'src/img/icons.svg#icon-minus-circle',
+  plusCircle: 'src/img/icons.svg#icon-plus-circle',
+  user: 'src/img/icons.svg#icon-user',
+  bookmarkFill: 'src/img/icons.svg#icon-bookmark-fill',
+  check: 'src/img/icons.svg#icon-check',
+  arrowRight: 'src/img/icons.svg#icon-arrow-right',
+  search: 'src/img/icons.svg#icon-search',
+  loader: 'src/img/icons.svg#icon-loader',
+  alertCircle: 'src/img/icons.svg#icon-alert-circle',
+  alertTriangle: 'src/img/icons.svg#icon-alert-triangle',
+  arrowLeft: 'src/img/icons.svg#icon-arrow-left',
+  bookmark: 'src/img/icons.svg#icon-bookmark',
+  edit: 'src/img/icons.svg#icon-edit',
+  send: 'src/img/icons.svg#icon-send',
+  smile: 'src/img/icons.svg#icon-smile',
+  uploadCloud: 'src/img/icons.svg#icon-upload-cloud',
+};
+
+export default icons;
