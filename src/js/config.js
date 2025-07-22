71385bf5b122b8e1aee3227164ab92b32b02710d
Mahmoud-dev09 <mahmoud0114829@gmail.com>
2025-07-22 18:38:19 +0300

    feat: Handling error with slow and failed requests

--- src/js/config.js
+++ src/js/config.js
@@ -1,1 +1,2 @@
 export const API_URL = 'https://forkify-api.jonas.io';
+export const TIMEOUT_SEC = 10;
--- src/js/helper.js
+++ src/js/helper.js
@@ -1,7 +1,18 @@
+import async from 'regenerator-runtime';
+import { TIMEOUT_SEC } from './config.js';
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
 export const getJSON = async function (url) {
   try {
-    const res = await fetch(url);
+    const res = await Promise.race([fetch(url), timeout(0.001)]);
     const data = await res.json();
+
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
     return data;
   } catch (err) {
