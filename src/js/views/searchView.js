f6d368f6b3556b32f7f16b2a7f4f17a77f3ffd51
Mahmoud-dev09 <mahmoud0114829@gmail.com>
2025-07-29 00:44:53 +0300

    Add clearInput  method

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -5,6 +5,10 @@ class SearchView {
     return this.#parentElement.querySelector('.search__field').value;
   }
 
+  clearInput() {
+    this.#parentElement.querySelector('.search__field').value = '';
+  }
+
   addHandlerRender(handler) {
     this.#parentElement.addEventListener('submit', function (e) {
       e.preventDefault();
