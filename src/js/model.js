15f61b47b935aac221bb63039671ce5b15d4d1f5
Mahmoud-dev09 <mahmoud0114829@gmail.com>
2025-07-28 23:51:05 +0300

    Add LoadSearchResults

--- src/js/model.js
+++ src/js/model.js
@@ -1,8 +1,13 @@
 import { async } from 'regenerator-runtime';
 import { API_URL } from './config.js';
 import { getJSON } from './helpers.js';
+
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
@@ -27,3 +32,27 @@ export const loadRecipe = async function (id) {
     throw err;
   }
 };
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}search?q=${query}`);
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+    console.log(state.search.results);
+  } catch (err) {
+    // Temp error handling
+    console.error(`${err} 💥💥💥💥`);
+    throw err;
+  }
+};
+
+loadSearchResults('pasta');
--- src/js/recipeView.js
+++ src/js/recipeView.js
@@ -1,6 +1,8 @@
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
+  #errorMessage = 'We could not found that recipe. Please try again.';
+  #message = '';
 
   render(data) {
     this.#data = data;
@@ -24,12 +26,27 @@ class RecipeView {
     this.#parentElement.insertAdjacentElementHTML('afterbegin', markup);
   }
 
-  renderError(message) {
+  renderError(message = this.#errorMessage) {
     const markup = `
       <div class="error">
         <div>
           <svg>
             <use href = "${icons}#icon-alert-triangle"></use>
+           </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+      `;
+    this.#clear();
+    this.#parentElement.insertAdjacentElementHTML('afterbegin', markup);
+  }
+
+  renderMessage(message = this.#message) {
+    const markup = `
+      <div class="message">
+        <div>
+          <svg>
+            <use href = "${icons}#icon-smile"></use>
            </svg>
         </div>
         <p>${message}</p>
